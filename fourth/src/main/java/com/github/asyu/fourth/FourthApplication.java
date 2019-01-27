5b19704814605f4815336b7caab70962510918fe
asyu <kahlman@naver.com>
2019-01-27 22:22:01 +0900

    Study Commit(Fourth 2)

---
+++ fourth/src/main/java/com/github/asyu/fourth/FourthApplication.java
@@ -0,0 +1,40 @@
+package com.github.asyu.fourth;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FourthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourthApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring";
+    }
+
+    /*
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
+    */
+}
+
---
+++ second/src/main/java/com/gitgub/asyu/second/SecondApplication.java
@@ -0,0 +1,16 @@
+package com.gitgub.asyu.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class SecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondApplication.class, args);
+    }
+
+
+}
+
